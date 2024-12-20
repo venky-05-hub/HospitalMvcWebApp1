41c477db3c755e2b18215b0171426a33a647f2b0
H P <H P@DESKTOP-ULC72CR>
2024-12-20 12:02:00 +0530

    just get only

---
+++ HospitalMvcWebApp1/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using HospitalMvcWebApp1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalMvcWebApp1.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public ActionResult GetMembers()
+        {
+            IEnumerable<DoctorsRegistrationModel> doctors = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7258/api/");
+
+                  
+                var responseTask = client.GetAsync("Doctors");
+                responseTask.Wait();
+
+                //To store result of web api response.   
+                var result = responseTask.Result;
+
+                //If success received   
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<DoctorsRegistrationModel>>();
+                    readTask.Wait();
+
+                    doctors = readTask.Result;
+                }
+                else
+                {
+                    //Error response received   
+                    doctors = Enumerable.Empty<DoctorsRegistrationModel>();
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+            return View(doctors);
+        }
+       
+
+    }
+}
---
+++ HospitalMvcWebApp1/Models/DoctorsRegistrationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HospitalMvcWebApp1.Models
+{
+    public class DoctorsRegistrationModel
+    {
+        public Guid DId { get; set; }
+        public string? DoctorsName { get; set; }
+
+        public long MobileNo { get; set; }
+
+        public string? Specialization { get; set; }
+        public string? Degrees { get; set; }
+    }
+}
